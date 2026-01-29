a02bfcfc822e952a7937c0a4d57e95c7512366a7
Michael Büsch <m@bues.ch>
2026-01-29 21:22:46 +0100

    Add release script

---
+++ scripts/makerelease.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# -*- coding: utf-8 -*-
+
+srcdir="$(realpath "$0" | xargs dirname)"
+srcdir="$srcdir/.."
+
+# Import the makerelease.lib
+# https://bues.ch/cgit/misc.git/tree/makerelease.lib
+die() { echo "$*"; exit 1; }
+for path in $(echo "$PATH" | tr ':' ' '); do
+    [ -f "$MAKERELEASE_LIB" ] && break
+    MAKERELEASE_LIB="$path/makerelease.lib"
+done
+[ -f "$MAKERELEASE_LIB" ] && . "$MAKERELEASE_LIB" || die "makerelease.lib not found."
+
+hook_get_version()
+{
+    version="$(cargo_local_pkg_version periodic-audit)"
+}
+
+project=periodic-audit
+conf_upload_packages="periodic-audit"
+makerelease "$@"
+
+# vim: ts=4 sw=4 expandtab
